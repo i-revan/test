0e1b559188c900b4294998813567415681ebc2cc
i-revan <rvaniskndrov03@gmail.com>
2023-02-23 12:22:59 +0400

    created js and c# file

---
+++ main.js
@@ -0,0 +1,23 @@
+let x = 5;
+let y = 8;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(y%x);
+console.log(++x);
+console.log(--y);
+y += x;
+console.log(y);
+x *= y
+console.log(x);
+let text1 = "What a"
+let text2 = " nice day!"
+text3 = text1 + text2
+console.log(text3);
+bool1 = true;
+bool2 = false;
+console.log(bool1 && bool2);
+console.log(bool1 || bool2);
+console.log(!bool1);
+console.log(y**x);
