0a151a5221ea05ad84bf8b190570f98ee7dc4c87
i-revan <rvaniskndrov03@gmail.com>
2023-02-23 12:12:50 +0400

    created consoleapp

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 8;
+            int y = 2;
+            Console.WriteLine(x*y);
+            Console.WriteLine(Math.Pow(y,x));
+            Console.WriteLine(!true);
+        }
+    }
+}
